cfbd9a792ea9811ff00512d29075d48a2b486908
foxJ <backtothefuture3030@gmail.com>
2022-04-21 14:23:02 +0900

    Cleaning Js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from "react" ;
-
 import Sweet from "components/Sweet"
-import { v4 as uuidv4 } from "uuid";
 import { dbService, storageService } from "fbase";
+import SweetFactory from "components/SweetFactory";
 
 const Home= ({userObj}) => {
-    const [sweet, setSweet] = useState("");
     const [sweets, setSweets] = useState([]);
-    const [attachment, setAttachment] = useState("")
     useEffect(() => {
         dbService.collection("sweets").onSnapshot(snapshot => {
             const sweetArray = snapshot.docs.map(doc =>({
@@ -17,63 +14,9 @@ const Home= ({userObj}) => {
             setSweets(sweetArray);
         });
     }, []);
-    const onSubmit = async (event) =>{
-        event.preventDefault();
-        let attachmentUrl = "";
-        if(attachment !== ""){
-            const attachmentRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
-            const response = await attachmentRef.putString(attachment, "data_url");
-            attachmentUrl = await response.ref.getDownloadURL();
-        }
-        const sweetObj = { 
-            text:sweet,
-            createdAt: Date.now(),
-            craetorId : userObj.uid,
-            attachmentUrl,
-        };
-        await dbService.collection("sweets").add(sweetObj);
-         setSweet("");
-         setAttachment("");
-    };
-    const onChange = (event)=>{
-        const { target:{value},
-    } = event;
-    setSweet(value);
-    };
-    const onFileChange = (event) => {
-        const {
-          target: { files },
-        } = event;
-        const theFile = files[0];
-        const reader = new FileReader();
-        reader.onloadend = (finishedEvent) => {
-            const {
-                currentTarget: { result },
-              } = finishedEvent;
-              setAttachment(result);
-            };
-            reader.readAsDataURL(theFile);
-          };
-    const onClearAttactment = () => setAttachment(null)
-
     return (
     <div>
-        <form onSubmit={onSubmit}>
-            <input 
-            value={sweet} 
-            onChange={onChange} 
-            type="text" 
-            placeholder="what's on your mind?" 
-            maxLength={120} />
-            <input type="file" accept="image/*" onChange={onFileChange} />
-            <input type="submit" value="Sweet" />
-            {attachment && (
-            <div>
-                <img src={attachment} width="50px" height="50px" />
-                <button onClick={onClearAttactment}>Clear</button>
-            </div>
-                )}
-        </form>
+        <SweetFactory userObj={userObj}/>
         <div>
             {sweets.map((sweet) => (
                 <Sweet key={sweet.id} sweetObj={sweet} isOwner={sweet.craetorId === userObj.uid}/>
